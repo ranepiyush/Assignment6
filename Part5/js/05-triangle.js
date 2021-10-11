2e656069c6b8498f6c89033a67163eb55e53ee4a
ranepiyush <89864143+ranepiyush@users.noreply.github.com>
2021-10-10 21:35:39 -0700

    Add files via upload
    This is the sixth assignment

---
+++ Part5/js/05-triangle.js
@@ -0,0 +1,9 @@
+var i;
+for(i=0;i<7;i++)
+{
+    for(j=0;j<=i;j++)
+    {
+        window.document.write("#");
+    }
+    window.document.write("<br>");
+}
---
+++ Part8/js/08-countdown.js
@@ -0,0 +1,7 @@
+var count;
+var i;
+count=parseInt(window.prompt("Enter the number"));
+for(i=count;i>=0;i--)
+{
+    window.document.write(i+"<br>");
+}
